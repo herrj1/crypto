c2c9430ae60b9d2eb848d077b1372d564aff9b3d
Jonathan <38092600+herrj1@users.noreply.github.com>
2018-08-08 23:47:12 -0400

    Update app.py

--- app2/app.py
+++ app2/app.py
@@ -9,8 +9,6 @@ button1.pack()
 button1.bind('<Button-1>',stopProg)
 root.mainloop()
 
-
-
 from tkinter import *
 from Crypto.Cipher import AES
 
@@ -21,7 +19,7 @@ def Encrypt(e):
 
 #Decrypt
 def Decrypt(e):
-    #obj = AES.new('This is a key123', AES.MODE_CBC, 'This is an IV456')
+    #obj = AES.new('This is a key8765', AES.MODE_CBC, 'This is an IV456')
     label1.configure(text="Encrypt", fg="red")
 
 #Import tkinter library into the program   
@@ -30,7 +28,6 @@ root=Tk()
 encrypt=Button(root,text="Encrypt")
 encrypt.pack(side = RIGHT)
 encrypt.bind('<Button-1>',Encrypt)
-
 
 decrypt=Button(root,text="Decrypt")
 decrypt.pack(side = LEFT)
