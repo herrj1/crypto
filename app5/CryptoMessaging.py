81c47cf4e44c3f7d93692d2bc08f141feb21a780
Jonathan <38092600+herrj1@users.noreply.github.com>
2019-07-11 23:03:05 -0400

    Update CryptoMessaging.py

--- app5/CryptoMessaging.py
+++ app5/CryptoMessaging.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*--
-# Form implementation generated from reading ui file 'C:\Users\xxxxx\Documents\xxxxxxxxxx\py\Crypto Messaging.ui'
+# Form implementation generated from reading ui file 'C:\Users\xxxx\Documents\xxxxxx\py\Crypto Messaging.ui'
 # Created by: PyQt5 UI code generator 5.10
 # WARNING! All changes made in this file will be lost er!
 from PyQt5 import QtCore, QtGui, QtWidgets
