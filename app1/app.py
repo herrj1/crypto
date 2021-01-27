169a0eae6f31c7ea3a077108a55d19ec73f4bbc3
Jonathan <38092600+herrj1@users.noreply.github.com>
2021-01-26 23:39:21 -0500

    Update app.py

--- app1/app.py
+++ app1/app.py
@@ -3,7 +3,7 @@ def stopProg(e):
     root.destroy()
 root=Tk()
 button1=Button(root,
- text="Hi World click to closes")
+ text="Hi World click to close")
 button1.pack()
 button1.bind('<Button-1>',stopProg)
 root.mainloop()
